e3bd06e0bac821ec610eb44e7bc6fa6d9aa7a386
Luis Velásquez <luvejo.dev@gmail.com>
2022-04-20 16:58:31 -0500

    build: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({});
