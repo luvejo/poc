fe47116dd053b0c72b3d966fda5873c02ef6b114
Luis Velásquez <luvejo.dev@gmail.com>
2022-06-17 10:19:24 -0500

    chore: add failing useInfiniteScroll implementation

--- app.vue
+++ app.vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { Autoplay } from "swiper";
+import { useInfiniteScroll } from "@vueuse/core";
 
-import "swiper/css";
+const el = ref<HTMLElement>(null);
+const data = ref([1, 2, 3, 4, 5, 6]);
+
+useInfiniteScroll(
+  el,
+  () => {
+    console.log("[*] loading more...");
+    data.value.push(Math.floor(Math.random() * 100));
+  },
+  { distance: 10 }
+);
 </script>
 
 <template>
-  <swiper
-    :spaceBetween="30"
-    :centeredSlides="true"
-    :modules="[Autoplay]"
-    :autoplay="{
-      delay: 4000,
-    }"
-  >
-    <swiper-slide>1</swiper-slide>
-    <swiper-slide>2</swiper-slide>
-    <swiper-slide>3</swiper-slide>
-  </swiper>
+  <div ref="el" class="container">
+    <div v-for="item in data">
+      {{ item }}
+    </div>
+  </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  padding: 1rem;
+  border-radius: 0.25rem;
+  gap: 0.5rem;
+  width: 300px;
+  height: 300px;
+}
+</style>
